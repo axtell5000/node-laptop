fbe0ac3f960a7aaf1b09859f6c942b814b19a1d1
Stephen Axtell <axtell.stephen@gmail.com>
2019-03-28 12:49:27 +0200

    commiting the finished project

---
+++ index.js
@@ -0,0 +1,71 @@
+const fs = require('fs');
+const http = require('http');
+const url = require('url');
+
+const json = fs.readFileSync(`${__dirname}/data/data.json`, 'utf-8');
+const laptopData = JSON.parse(json);
+
+const server = http.createServer((req, res) => {
+
+  const pathname = url.parse(req.url).pathname;
+  const queryId = url.parse(req.url, true).query.id;
+
+  console.log(pathname);
+
+  if (pathname === '/products' || pathname === '/') {
+    res.writeHead(200, {'Content-type': 'text/html'});
+    fs.readFile(`${__dirname}/templates/template-overview.html`, 'utf-8', (err, data) => {
+      let overviewOutput = data;
+
+      fs.readFile(`${__dirname}/templates/template-card.html`, 'utf-8', (err, data) => {
+        const cardsOutput = laptopData.map(el => replaceTemplate(data, el)).join('');
+        overviewOutput = overviewOutput.replace('{%CARDS%}', cardsOutput);
+
+        res.end(overviewOutput);
+      });
+
+    });
+
+  } else if ((/\.(jpg|jpeg|png|gif)$/i).test(pathname)) {
+    fs.readFile(`${__dirname}/data/img${pathname}`, (err, data) => {
+      res.writeHead(200, {'Content-type': 'image/jpg'});
+      res.end(data);
+    });
+  } else if (pathname === '/css/style.css') {
+    fs.readFile(`${__dirname}/css/style.css`, (err, data) => {
+      res.writeHead(200, {'Content-type': 'text/css'});
+      res.end(data);
+    });
+  }
+
+  else if (pathname === '/laptop' && queryId < laptopData.length) {
+    res.writeHead(200, {'Content-type': 'text/html'});
+    fs.readFile(`${__dirname}/templates/template-laptop.html`, 'utf-8', (err, data) => {
+      const laptop = laptopData[queryId];
+      const output = replaceTemplate(data, laptop);
+      res.end(output);
+    });
+  } else {
+    res.writeHead(404, {'Content-type': 'text/html'});
+    res.end('URL wasnt found on the server');
+  }
+
+
+});
+
+server.listen(1337, '127.0.0.1', () => {
+  console.log("Server now listening");
+});
+
+const replaceTemplate = (originalHtml, laptop) => {
+  let output = originalHtml.replace(/{%PRODUCTNAME%}/g, laptop.productName);
+  output = output.replace(/{%IMAGE%}/g, laptop.image);
+  output = output.replace(/{%PRICE%}/g, laptop.price);
+  output = output.replace(/{%SCREEN%}/g, laptop.screen);
+  output = output.replace(/{%CPU%}/g, laptop.cpu);
+  output = output.replace(/{%STORAGE%}/g, laptop.storage);
+  output = output.replace(/{%RAM%}/g, laptop.ram);
+  output = output.replace(/{%DESCRIPTION%}/g, laptop.description);
+  output = output.replace(/{%ID%}/g, laptop.id);
+  return output;
+};
